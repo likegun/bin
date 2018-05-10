91e2ef97f4716333ce6f55a291ab1a8218c86315
wrq <likegun94@gmail.com>
2018-05-10 16:40:22 +0800

    改用inquirer

--- header.js
+++ header.js
@@ -1,34 +1,43 @@
 #!/usr/bin/env node
 'use strict'
-const fs = require('fs');
-
-function done(msg) {
-    console.log(msg);
-    process.exit();
-}
-
-if(process.argv.length < 3) done('你倒是给文件呀!');
-const targetFile = process.argv[2];
-const fuhao = process.argv[3] ? process.argv[3] : '\'';
-
-let headers;
-try {
-    headers = fs.readFileSync(targetFile).toString().split('\n');
-} catch(e) {
-    done(`读取文件错误:${e.message}`);
-}
+var inquirer = require('inquirer');
+let f;
 
 function transfer(headers) {
     const ret = [];
-    for(let header of headers) {
+    for(let header of headers.filter(e => e)) {
         const index = header.indexOf(':');
-        ret.push(`${fuhao}${header.slice(0, index)}${fuhao}:${fuhao}${header.slice(index + 1).trim()}${fuhao}`)
+        ret.push(`${f}${header.slice(0, index)}${f}:${f}${header.slice(index + 1).trim()}${f}`)
     }
     return ret.join(',\n');
 }
 
-try {
-    console.log(transfer(headers));
-} catch(e) {
-    done(`文件处理错误:${e.message}`);
-}
+(async () => {
+    f = (await inquirer.prompt([{
+        type: 'list',
+        name: 'f',
+        message: '要单引号还是双引号',
+        choices: [
+            {name: '\''},
+            {name: '"'}
+        ]
+    }])).f;
+    while(true) {
+        try {
+            const h = (await inquirer.prompt([{
+                type: 'editor',
+                name: 'h',
+                message: '输入headers',
+            }])).h;
+            console.log(transfer(h.split('\n')))
+        } catch(e) {
+            console.error(e);
+        }
+    }
+})();
+
+// try {
+//     console.log(transfer(headers));
+// } catch(e) {
+//     done(`文件处理错误:${e.message}`);
+// }
